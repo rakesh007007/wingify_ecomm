395ddefd9e13dce36d296f4462a61dc4868b76a6
rakesh.yadav@limetray.com <rakesh.yadav@limetray.com>
2016-03-30 02:41:25 +0530

    requirements.txt changed

--- automate.sh
+++ automate.sh
@@ -2,6 +2,12 @@ echo "installing the python....."
 sudo apt-get install python
 echo "pyhton has been installed"
 echo "installing the pip for python"
+
+sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
+
+sudo easy_install greenlet
+
+sudo easy_install gevent
 sudo apt-get install python-pip
 echo "done."
 echo "installing the virtualenv for python"
@@ -19,5 +25,5 @@ echo "running command django-admin  makemigrations app.."
 
 python wingify_ecomm/manage.py makemigrations app
 python wingify_ecomm/manage.py migrate
-python wingify_ecomm/manage.py loaddata wingify_ecomm/app/db.json
+python wingify_ecomm/manage.py loaddata app wingify_ecomm/app/db.json
 python wingify_ecomm/manage.py runserver 
